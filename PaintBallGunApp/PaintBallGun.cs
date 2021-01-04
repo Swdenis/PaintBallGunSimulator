338fa67a62c74520c0a05647d16967c1ac8e0993
Swdenis <swdenis@mail.ru>
2021-01-04 14:37:11 +0100

    * .gitignore: Initial commit
    * PaintBallGunApp.sln:
    * Program.cs:
    * PaintBallGun.cs:
    * PaintBallGunApp.csproj:

---
+++ PaintBallGunApp/PaintBallGun.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace PaintBallGunApp
+{
+    class PaintBallGun
+    {
+        public PaintBallGun(int balls, int magazineSize, bool loaded)
+        {
+            this.balls = balls;
+            MagazineSize = magazineSize;
+            if (!loaded) Reload();
+        }
+        public int MagazineSize { get; private set; } = 16;
+        private int balls = 0;
+        public int BallsLoaded { get; private set; }
+
+
+        public bool IsEmpty() { return BallsLoaded == 0; }
+
+        public int Balls
+        {
+            get { return balls; }
+            set { if (value > 0) balls = value;
+                Reload();
+            }
+        }
+
+        public void Reload()
+        {
+            if (balls > MagazineSize)
+                BallsLoaded = MagazineSize;
+            else BallsLoaded = balls;
+        }
+
+        public bool Shoot()
+        {
+            if (BallsLoaded == 0) return false;
+            BallsLoaded--;
+            balls--;
+            return true;
+        }
+
+
+
+        
+    }
+}
